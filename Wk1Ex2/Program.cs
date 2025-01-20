6eb85c100c2d6660c27453a4c8dae5b7411129ce
JasmineRamMil <j0561448@amarillocollege.com>
2025-01-19 21:04:57 -0600

    Update Program.cs

--- Wk1Ex2/Program.cs
+++ Wk1Ex2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your first name?");
+string firstName = Console.
+ReadLine();
+Console.WriteLine(firstName);
+Console.WriteLine("What is your last name?");
+string lastName = Console.
+ReadLine();
+Console.WriteLine(lastName);
+Console.WriteLine("What is your age?");
+string age = Console.
+ReadLine();
+Console.WriteLine(age);
+Console.WriteLine("What is your favorite color?");
+string favColor = Console.
+ReadLine();
+Console.WriteLine(favColor);
+Console.WriteLine("Hello "+ firstName+" "+ lastName+"! You are "+age+" years old and your favorite color is "+favColor);
